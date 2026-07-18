12764cbb632d7d2c9d88d115a4f1a9044b92b5bd
SeekAll Dev <dev@seekall.winmelon.cn>
2026-07-18 21:05:28 +0800

    fix(v0.5): dmca module 加 imports mailmodule 修依赖注入
    服务器启动报错:
    Nest can't resolve dependencies of the DmcaService
    (PrismaService, ?, Symbol(REDIS_CLIENT))
    Please make sure that the argument MailService at index [1]
    is available in the DmcaModule context.
    
    原因: DmcaModule 没 import MailModule, MailService 无法注入
    修复: dmca.module.ts imports 加 MailModule

--- apps/api/src/modules/dmca/dmca.module.ts
+++ apps/api/src/modules/dmca/dmca.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DmcaController, AdminDmcaController } from './dmca.controller'
 import { DmcaService } from './dmca.service'
+import { MailModule } from '../mail/mail.module'
 
 @Module({
+  imports: [MailModule],
   controllers: [DmcaController, AdminDmcaController],
   providers: [DmcaService],
   exports: [DmcaService],
